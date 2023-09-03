802bd39ee956cacf20e6cd08bec62c8ab7f4150e
AspectOfTheNoob <Mcthouacbb@gmail.com>
2023-09-02 22:21:13 -0400

    Bruh

--- Tune/src/extract.cpp
+++ Tune/src/extract.cpp
@@ -2,13 +2,13 @@
 
 void extractMaterial(const EvalParams& params)
 {
-    int material[2][6] = {};
+    std::array<std::array<int, 2>, 6> material;
     for (int pce = 1; pce < 6; pce++)
     {
         int mgAvg = 0;
         int egAvg = 0;
-	int startSquare = pce == 5 ? 24 : 0;
-	int endSquare = pce == 5 ? 56 : 64;
+        int startSquare = pce == 5 ? 24 : 0;
+        int endSquare = pce == 5 ? 56 : 64;
         for (int sq = startSquare; sq < endSquare; sq++)
         {
             mgAvg += params.data.psqtMG[pce * 64 + sq];
@@ -43,57 +43,110 @@ void extractMaterial(const EvalParams& params)
     printParams(EvalParams(data), std::cout);
 }
 
-void normBytes(EvalParams params)
+std::vector<uint128_t> compressParams(const EvalParams& params, const std::array<std::array<int, 6>, 2>& material)
 {
-	EvalData& data = params.data;
 
-	int material[2][6] = {};
+}
 
-	for (int i = 0; i < 6; i++)
-	{
-		int mgPsqtSum = 0;
-		int egPsqtSum = 0;
-		for (int j = 0; j < 64; j++)
-		{
-			mgPsqtSum += data.psqtMG[i * 64 + j];
-			egPsqtSum += data.psqtEG[i * 64 + j];
-		}
+void normBytes(EvalParams& params, std::array<std::array<int, 6>, 2>& material)
+{
+    EvalData& data = params.data;
 
-		while (mgPsqtSum > 32)
-		{
-			mgPsqtSum -= 64;
-			for (int j = 0; j < 64; j++)
-				data.psqtMG[i * 64 + j] -= 1;
-			if (i != 0)
-				material[0][i] += 1;
-		}
-		while (mgPsqtSum <= -32)
-		{
-			mgPsqtSum += 64;
-			for (int j = 0; j < 64; j++)
-				data.psqtMG[i * 64 + j] += 1;
-			if (i != 0)
-				material[0][i] -= 1;
-		}
+    material = {};
 
-		while (egPsqtSum > 32)
-		{
-			egPsqtSum -= 64;
-			for (int j = 0; j < 64; j++)
-				data.psqtEG[i * 64 + j] -= 1;
-			if (i != 0)
-				material[1][i] += 1;
-		}
-		while (egPsqtSum <= -32)
-		{
-			egPsqtSum += 64;
-			for (int j = 0; j < 64; j++)
-				data.psqtEG[i * 64 + j] += 1;
-			if (i != 0)
-				material[1][i] -= 1;
-		}
-	}
+    for (int i = 0; i < 6; i++)
+    {
+        int mgPsqtSum = 0;
+        int egPsqtSum = 0;
+        for (int j = 0; j < 64; j++)
+        {
+            mgPsqtSum += data.psqtMG[i * 64 + j];
+            egPsqtSum += data.psqtEG[i * 64 + j];
+        }
 
+        while (mgPsqtSum > 32)
+        {
+            mgPsqtSum -= 64;
+            for (int j = 0; j < 64; j++)
+                data.psqtMG[i * 64 + j] -= 1;
+            if (i != 0)
+                material[0][i] += 1;
+        }
+        while (mgPsqtSum <= -32)
+        {
+            mgPsqtSum += 64;
+            for (int j = 0; j < 64; j++)
+                data.psqtMG[i * 64 + j] += 1;
+            if (i != 0)
+                material[0][i] -= 1;
+        }
+
+        while (egPsqtSum > 32)
+        {
+            egPsqtSum -= 64;
+            for (int j = 0; j < 64; j++)
+                data.psqtEG[i * 64 + j] -= 1;
+            if (i != 0)
+                material[1][i] += 1;
+        }
+        while (egPsqtSum <= -32)
+        {
+            egPsqtSum += 64;
+            for (int j = 0; j < 64; j++)
+                data.psqtEG[i * 64 + j] += 1;
+            if (i != 0)
+                material[1][i] -= 1;
+        }
+    }
+
+    int min = INT_MAX;
+    int max = INT_MIN;
+    for (int* p = reinterpret_cast<int*>(data.psqtMG); p != params.params + NUM_PARAMS; p++)
+    {
+        if (*p > max)
+            max = *p;
+        if (*p < min)
+            min = *p;
+    }
+
+    int range = max - min;
+    std::cout << "Mult Factor: " << 255 << " / " << range << std::endl;
+
+    int diff = 0;
+    // too lazy to figure out the mathematical approach without loops
+    while (min < 0)
+    {
+        diff++;
+        min++;
+        max++;
+    }
+    while (min > 0)
+    {
+        diff--;
+        min--;
+        max--;
+    }
+
+    std::cout << "New min: " << min << " New max: " << max << " diff: " << diff << std::endl;
+
+    for (int i = 1; i < 6; i++)
+    {
+        material[0][i] -= diff;
+        material[0][i] = static_cast<int>(std::round(material[0][i] * 255.0 / range));
+        material[1][i] -= diff;
+        material[1][i] = static_cast<int>(std::round(material[1][i] * 255.0 / range));
+    }
+
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < 64; j++)
+        {
+            data.psqtMG[i * 64 + j] += diff;
+            data.psqtMG[i * 64 + j] = static_cast<int>(std::round(data.psqtMG[i * 64 + j] * 255.0 / range));
+            data.psqtEG[i * 64 + j] += diff;
+            data.psqtEG[i * 64 + j] = static_cast<int>(std::round(data.psqtEG[i * 64 + j] * 255.0 / range));
+        }
+    }
 
     for (int i = 0; i < 2; i++)
     {
@@ -104,66 +157,5 @@ void normBytes(EvalParams params)
         }
         std::cout << std::endl;
     }
-	printParams(params, std::cout);
-	
-
-	int min = INT_MAX;
-	int max = INT_MIN;
-	for (int* p = reinterpret_cast<int*>(data.psqtMG); p != params.params + NUM_PARAMS; p++)
-	{
-		if (*p > max)
-			max = *p;
-		if (*p < min)
-			min = *p;
-	}
-
-	int range = max - min;
-	std::cout << "Mult Factor: " << 255 << " / " << range << std::endl;
-
-	int diff = 0;
-	// too lazy to figure out the mathematical approach without loops
-	while (-min > max)
-	{
-		diff++;
-		min++;
-		max++;
-	}
-	while (-min < max)
-	{
-		diff--;
-		min--;
-		max--;
-	}
-
-	std::cout << "New min: " << min << " New max: " << max << " diff: " << diff << std::endl;
-
-	for (int i = 1; i < 6; i++)
-	{
-		material[0][i] -= diff;
-		material[0][i] = static_cast<int>(std::round(material[0][i] * 255.0 / range));
-		material[1][i] -= diff;
-		material[1][i] = static_cast<int>(std::round(material[1][i] * 255.0 / range));
-	}
-
-	for (int i = 0; i < 6; i++)
-	{
-		for (int j = 0; j < 64; j++)
-		{
-			data.psqtMG[i * 64 + j] += diff;
-			data.psqtMG[i * 64 + j] = static_cast<int>(std::round(data.psqtMG[i * 64 + j] * 255.0 / range));
-			data.psqtEG[i * 64 + j] += diff;
-			data.psqtEG[i * 64 + j] = static_cast<int>(std::round(data.psqtEG[i * 64 + j] * 255.0 / range));
-		}
-	}
-
-    for (int i = 0; i < 2; i++)
-    {
-        std::cout << material[i][0];
-        for (int j = 1; j < 6; j++)
-        {
-            std::cout << ", " << material[i][j];
-        }
-        std::cout << std::endl;
-    }
-	printParams(params, std::cout);
+    printParams(params, std::cout);
 }
--- Tune/src/extract.h
+++ Tune/src/extract.h
@@ -1,4 +1,8 @@
 #include "eval_params.h"
+#include <uint128_t/uint128_t.h>
+
+#include <array>
 
 void extractMaterial(const EvalParams& params);
-void normBytes(EvalParams params);
+void normBytes(EvalParams& params, std::array<std::array<int, 6>, 2>& material);
+std::vector<uint128_t> compressParams(const EvalParams& params, const std::array<std::array<int, 6>, 2>& material);
